9ca94aa49d9d307b393229a0c18ed87c72f03d24
rodrigo <rodrigomiliano16@gmail.com>
2020-11-05 23:10:57 -0300

    Cambios segùn clase fin Octubre

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Axios from 'axios';
+import React, {Component} from 'react';
+import { StyleSheet, TextInput, Button, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const axios = require('axios');
+
+export default class App extends Component {
+
+  handlerButton(){
+    var nombre = this.state.nombre;
+    Axios.get("https://anapioficeandfire.com/api/characters", {params:{name:this.state.nombre}})
+    .then(response => {
+      console.log(response.data);
+      this.setState(() => {return {consuleApi:true, data: response.data}});
+    })
+    .catch(error => {
+      console.log(error);
+    });
+
+    console.log("Me clickearon");
+  }
+
+  handlerTxt(text){
+    console.log("Escribieron " + text);
+    this.setState({nombre: text});
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 , width: 200}}
+          onChangeText={text => this.handlerTxt(text)}          
+        />
+        <Button
+          onPress={this.handlerButton.bind(this)}
+          title="Consultar API"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
